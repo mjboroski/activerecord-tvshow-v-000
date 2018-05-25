9144e19c062100041accd0b47dfb17e7e1fa7591
Michael Boroski <mjboroski@gmail.com>
2018-05-25 04:28:06 +0000

    automatically backed up by learn

---
+++ db/migrate/001_create_shows.rb
@@ -0,0 +1,10 @@
+class CreateShows < ActiveRecord::Migration
+  def change
+    create_table :shows do |t|
+      t.string :name
+      t.string :network
+      t.datetime :day
+      t.integer :rating
+  end
+
+end
